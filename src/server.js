fb5cbbb7f05e8146bc5dbef95c814f3c24332db3
Daniil Balashov <danil.balashik.98@gmail.com>
2022-11-09 19:55:51 +0300

    name db

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
-import session from 'express-session';
-import store from 'session-file-store';
 import jsxRender from './utils/jsxRender';
 import indexRouter from './routes/indexRouter';
+import apiRouter from './routes/apiRouter';
 
 const app = express();
 const PORT = 3000;
@@ -18,27 +17,12 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(morgan('dev'));
 
-const FileStore = store(session);
-
-const sessionConfig = {
-  name: 'user_sid', // Имя куки для хранения id сессии. По умолчанию - connect.sid
-  store: new FileStore(),
-  secret: 'oh klahoma', // Секретное слово для шифрования, может быть любым
-  resave: false, // Пересохранять ли куку при каждом запросе
-  saveUninitialized: false, // Создавать ли сессию без инициализации ключей в req.session
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 12, // Срок истечения годности куки в миллисекундах
-    httpOnly: true, // Серверная установка и удаление куки, по умолчанию true
-  },
-};
-app.use(session(sessionConfig));
-
 app.use((req, res, next) => {
   res.locals.path = req.originalUrl;
-  res.locals.user = req.session.user;
   next();
 });
 
 app.use('/', indexRouter);
+app.use('/api/v1/', apiRouter);
 
 app.listen(PORT, () => console.log(`Server is started on port ${PORT}`));
